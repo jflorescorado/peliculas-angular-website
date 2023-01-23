6646fd047e22615f334c37209b373e10f83265b7
Jose <jflorescorado03@gmail.com>
2023-01-23 02:02:51 -0600

    update and config .guard, app.component and pages

--- src/app/services/admin.guard.ts
+++ src/app/services/admin.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+
+  constructor(private loginService:LoginService, private router:Router){
+
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if(this.loginService.isLoggedIn() && this.loginService.getUserRol () == 'ADMIN'){
+      return true;
+    }
+      
+    this.router.navigate(['login']);
+    return false;
   }
   
 }
--- src/app/services/normal.guard.ts
+++ src/app/services/normal.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NormalGuard implements CanActivate {
+
+  constructor(private loginService:LoginService, private router:Router){
+
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if(this.loginService.isLoggedIn() && this.loginService.getUserRol() == 'NORMAL'){
+      return true;
+    }
+    this.router.navigate(['login']);
+    return false;
   }
   
 }
+
